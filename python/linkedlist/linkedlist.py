a9e66f3cd862f3579af1d2b694b1410aee94b98f
kumar <juniourkawasakicool@mgial.com>
2019-12-16 16:01:11 +0530

    Python: Linkedlist, added delete_nth_node_from_end.py

--- python/linkedlist/linkedlist.py
+++ python/linkedlist/linkedlist.py
@@ -33,7 +33,7 @@ class LinkedList:
     def element_count(self):
         print("Total element: " + str(self.n + 1))
 
-    def delete(self, i):
+    def delete_on_pos(self, i):
         cur = self.head
         if i>=0 and i<=self.n:
             if i == 0:
@@ -46,16 +46,46 @@ class LinkedList:
                 while(nex):
                     if i == k:
                         pre.next = cur.next
+                        cur = None
+                        break
                     else:
                         pre = cur
                         cur = cur.next
                         nex = cur.next
                     k +=1
+            self.n -= 1      
                 
         else:
             print("Please provide valid position")
             i = int(input("Position: "))
             self.delete(i)
+
+    def delete_on_value(self, k):
+        if self.head.value == k:
+            self.head = self.head.next
+        else:
+            cur = self.head.next
+            pre, nex = self.head, cur.next
+            while cur:
+                if cur.value == k:
+                    pre.next = cur.next
+                    cur = None
+                    self.n -= 1
+                    break
+                pre = cur
+                cur = nex
+                nex = nex.next
+
+
+    def reverse(self):
+        cur = self.head
+        pre, nex = None, None
+        while cur:
+            nex = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nex
+        self.head = pre
 
 def main():
     intro = """
@@ -63,7 +93,9 @@ def main():
     2 -> list elements
     3 -> No of nodes
     4 -> search an element
-    5 - > Delete an element at position
+    5 -> Delete a Node at position
+    6 -> Delete a node on value
+    7 -> Reverse the linked list
     99 - >  Quit"""
     obj = LinkedList()
     while True:
@@ -82,7 +114,14 @@ def main():
                 obj.element_count()
             if n == 5:
                 i = int(input("Provide position: "))
-                obj.delete(i)
+                obj.delete_on_pos(i)
+            if n == 6:
+                print("Elements: ")
+                obj.display()
+                i = int(input("Provide value to be deleted: "))
+                obj.delete_on_value(i)
+            if n == 7:
+                obj.reverse()
                 
         except Exception as e:
             print("Input should be from listed options", e)
